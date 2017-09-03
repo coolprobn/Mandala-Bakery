e7d567b369a4f8af14fb7d2f71e727e1844a5207
Prakash Poudel <prakashpoudel@lftechnology.com>
2017-09-03 18:51:27 +0545

    session added

--- App/components/register/register.js
+++ App/components/register/register.js
@@ -5,24 +5,65 @@ import {
   Text,
   View,
   TextInput,
-  Button, TouchableOpacity, Image
+  Button, TouchableOpacity, Image, AsyncStorage
 } from 'react-native';
+import axios from 'axios'
+const API_URL = "http://192.168.2.52:3000/";
 
 export default class Register extends Component {
+
+  constructor(){
+    super();
+    this.signUp = this.signUp.bind(this)
+    this.state = {
+      firstName: '',
+      secondName: '',
+      mobileNumber: '',
+      password: '',
+      confirmPassword:''
+    }
+  }
+
+  setValue(){
+
+  }
+
+  signUp(){
+    let signup_url = API_URL+ 'auth/sign_up'
+    console.log(signup_url)
+    axios.post(signup_url, {
+      first_Name: this.state.firstName,
+      second_Name: this.state.secondName,
+      mobile_Number: this.state.mobileNumber,
+      password: this.state.firstName,
+    }).then((result)=>{
+      console.log(result)
+    //  store value in android
+      AsyncStorage.multiSet([
+        ["firstName", this.state.firstName],
+        ["secondName", this.state.secondName],
+        ["mobileNumber", this.state.mobileNumber],
+        ["password", this.state.password],
+
+      ])
+
+    })
+  }
+
   render() {
     return (
       <Image source={require('../../images/coffeeBean1.jpg')} style={styles.registerContainer}>
 
         <View style={styles.text}>
 
-          <TextInput underlineColorAndroid={'rgba(0,0,0,0)'} style={styles.textInput} placeholder="First Name"/>
-          <TextInput underlineColorAndroid={'rgba(0,0,0,0)'} style={styles.textInput} placeholder="Last Name"/>
-          <TextInput underlineColorAndroid={'rgba(0,0,0,0)'} style={styles.textInput} placeholder="E-mail"/>
-          <TextInput underlineColorAndroid={'rgba(0,0,0,0)'} style={styles.textInput} placeholder="Password" secureTextEntry={true}/>
-          <TextInput underlineColorAndroid={'rgba(0,0,0,0)'} style={styles.textInput} placeholder="Confirm Password" secureTextEntry={true}/>
+          <TextInput underlineColorAndroid={'rgba(0,0,0,0)'} style={styles.textInput} placeholder="First Name" onChangeText={(firstName) => this.setState({firstName})}/>
+          <TextInput underlineColorAndroid={'rgba(0,0,0,0)'} style={styles.textInput} placeholder="Last Name" onChangeText={(secondName) => this.setState({secondName})}/>
+          <TextInput underlineColorAndroid={'rgba(0,0,0,0)'} style={styles.textInput} placeholder="Mobile Number" onChangeText={(mobileNumber) => this.setState({mobileNumber})}/>
+          <TextInput underlineColorAndroid={'rgba(0,0,0,0)'} style={styles.textInput} placeholder="Password" secureTextEntry={true} onChangeText={(password) => this.setState({password})}/>
+          <TextInput underlineColorAndroid={'rgba(0,0,0,0)'} style={styles.textInput} placeholder="Confirm Password" secureTextEntry={true} onChangeText={(confirmPassword) => this.setState({confirmPassword})}/>
 
           <TouchableOpacity  style={styles.register}>
-            <Text style={{textAlign: 'center', paddingVertical: 5, fontSize: 15}}>Register</Text>
+            <Text style={{textAlign: 'center', paddingVertical: 5, fontSize: 15}} onPress={this.signUp}>Register</Text>
           </TouchableOpacity>
 
         </View>
@@ -48,7 +89,7 @@ const styles = StyleSheet.create({
   textInput: {
     height: 50,
     marginBottom: 10,
-    backgroundColor: 'rgba(255,255,255,0.5)',
+    backgroundColor: 'rgba(255,255,255,0.7)',
   },
 
   register: {

d63d75fe56fe2211810b2b447c6087dd5274a581
Prakash Poudel <prakashpoudel@lftechnology.com>
2017-11-21 15:16:43 +0545

    remote server

--- App/config/config.js
+++ App/config/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-  API_URL: 'http://192.168.2.52:3000/',
+  API_URL: 'https://mbs-server.herokuapp.com/'
 };
+// API_URL: 'http://192.168.2.52:3000/',
 // API_URL: 'https://mbs-server.herokuapp.com/'
 export default CONFIG;
